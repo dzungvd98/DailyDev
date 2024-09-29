81b28230664a4973509a9b62e6e6b3437c6a19db
dzungvd98 <yungvd98@gmail.com>
2024-09-29 23:05:31 +0700

    Create spring boot project for daily dev

---
+++ src/main/java/com/application/daily_dev/DailyDevApplication.java
@@ -0,0 +1,13 @@
+package com.application.daily_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyDevApplication.class, args);
+	}
+
+}
